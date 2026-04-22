e8ef7967c9e2515ff59f8755faacaac675df5ff9
Vibesdk <vibesdk-bot@cloudflare.com>
2026-04-22 03:16:22 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add vite-plugin-pwa@^0.20.5"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
